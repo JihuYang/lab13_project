6018a7e52ada1e66497ed338f4d842e454eac4fb
JeongWonHA <61004941+JeongWonHA@users.noreply.github.com>
2020-04-29 23:18:50 +0900

    Define some function at fleamarket.h

--- fleamarket.h
+++ fleamarket.h
@@ -1,1 +1,7 @@
-
+LoadData_product(product Product*);
+SignUp();
+SaveData_Member(char *id);
+LoadData_Member(char *id);
+ReviewBoard(char *review);
+SaveData_Review(char *review);
+LoadData_Review(char *review);
