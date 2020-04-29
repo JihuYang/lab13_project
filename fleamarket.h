50190e822db64e65df47ed1e2854e5735a714af0
JihuYang <63491218+JihuYang@users.noreply.github.com>
2020-04-29 23:26:35 +0900

    Update fleamarket.h

--- fleamarket.h
+++ fleamarket.h
@@ -12,7 +12,11 @@ int createProduct(Product *p);  //제품명, 판매가격, 카테고리, 아이�
 void readProduct(Product *p);   //제품을 나열하기 위한 함수
 int updateProduct(Product *p);  //제품을 수정하기 위한 함수
 int deleteProduct(Product *p);  //제품을 삭제하기 위한 함수
+int selectDataNo(Product *p, int count) //수정하거나 삭제할 제품의 번호를 입력받는 함수
 int selectMenu();               //전체 메뉴를 보여주는 함수
 void listProduct(Product *p, int count); //제품 나열을 하기 위한 함수
 void saveProduct(Product *p, int count);     //제품 데이터 저장하기 위한 함수
 int loadProduct(Product *p);                 //제품 데이터 불러오기 위한 함수
+void searchName(Product *p, int count);      //제품 이름을 검색하는 함수
+void searchPrice(Product *p, int count);     //가격을 검색하는 함수
+void searchCategory(Product *p, int count);  //카테고리를 검색하는 함수
