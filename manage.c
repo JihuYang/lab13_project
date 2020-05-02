e21767360c0b9946f9f479067505629e24e3891c
Ha jeongwon <hajeongwon77@gmail.com>
2020-05-02 23:28:15 +0900

    manager.c

---
+++ manage.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "fleamarket.h"
+
+void Search(Product *p, int count){
+
+}
+
+void searchName(Product *p, int count){
+
+}
+
+void searchPrice(Product *p, int count){
+
+}
+
+void searchCategory(Product *p, int count){
+
+}
+
+void SavaData_Product(Product *p, int count){
+
+}
+
+int LoadData_Product(Product *p){
+
+}
