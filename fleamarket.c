1614a5709d34d2b36515467b35d4846c8c045ca0
Jihu Yang <63491218+JihuYang@users.noreply.github.com>
2020-05-03 23:28:28 +0900

    Update fleamarket.c

--- fleamarket.c
+++ fleamarket.c
@@ -1,1 +1,17 @@
 #include "fleamarket.h"
+int main(void){
+  
+    Product plist[100];
+    int curcount = 0;
+
+    Product p;
+    int count = 0, menu;
+    count = loadData(plist);
+    curcount = count;
+  
+    while (1){
+        menu = selectMenu();
+        if (menu == 0) break;
+    }
+  
+}
