19a0a069314ab8271969c9bc824165edf3237c33
Jihu Yang <63491218+JihuYang@users.noreply.github.com>
2020-05-04 23:07:56 +0900

    Update manage.c

--- manage.c
+++ manage.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 #include "fleamarket.h"
 
+int createProduct(Product *p){    //제품명, 판매가격, 카테고리, 아이디, 중고상품여부를 만들기 위한 함수
+  
+}
+
+void readProduct(Product *p){     //제품을 나열하기 위한 함수
+
+}
+
+
+int updateProduct(Product *p){    //제품을 수정하기 위한 함수
+  
+}
+int deleteProduct(Product *p){    //제품을 삭제하기 위한 함수
+
+}
+
+int selectDataNo(Product *p, int count){    //수정하거나 삭제할 제품의 번호를 입력받는 함수
+
+}
+
+int selectMenu(){    //전체 메뉴를 보여주는 함수
+
+}  
+  
+void listProduct(Product *p, int count){  //제품 나열을 하기 위한 함수
+
+}
+
+
+void saveProduct(Product *p, int count{   //제품 데이터 저장하기 위한 함수
+
+}
+
+int loadProduct(Product *p){     //제품 데이터 불러오기 위한 함수
+  
+}
+
 void Search(Product *p, int count){
 
 }
