d544be6c3b654f7bca2353b27d19aded2c80e53f
Ha jeongwon <hajeongwon77@gmail.com>
2020-05-07 18:53:12 +0900

    Fix all part 0507 PM 7:00

--- fleamarket.c
+++ fleamarket.c
@@ -1,4 +1,5 @@
 #include "fleamarket.h"
+#include <stdio.h>
 int main(void){
   
 	Product plist[100];
@@ -6,11 +7,16 @@ int main(void){
 
 	Product p;
 	int count = 0, menu;
+	char Memberlist [12][100];
+	char *id;
 	count = loadProduct(plist);
+	LoadData_Member(Memberlist);
+
 	curcount = count;
   
+	id = ID(Memberlist);
 	while (1){
-        	menu = selectMenu();
+        	menu = selectMenu(id);
         	if (menu == 0) break;
 		if (menu == 1){
 			if(count>0) listProduct(plist, curcount);
@@ -45,26 +51,26 @@ int main(void){
             		if(deleteProduct(&p) == 1) printf(" 삭제되었습니다!!\n");
         	}
 		else if (menu == 5){
+		//	Search(plist, curcount);	
+		}
+		
+		else if(menu == 6){
+			//Reviewboard();
+		}
+		else if (menu == 7){
+
 			if(count == 0) printf("등록할 중고 제품이 없습니다.\n");
 			else saveProduct(plist, curcount);
 		}
-		/*
-		else if(menu == 6){
-			LoadData_Review(char *review, int Review_count);
-		}
-		else if (menu == 7){
-			if(count == 0) printf("검색할 중고 제품이 없습니다.\n");
-			else searchName(plist, curcount);
-		}
 		else if (menu == 8){
-			if(count == 0) printf("검색할 중고 제품이 없습니디.\n");
-			else searchPrice(plist, curcount);
+			
+			SaveData_Member(id);
 		}
 		else if (menu == 9) {
 			if(count == 0) printf("검색할 중고 제품이 없습니다.\n");
-			else searchCategory(plist, curcount);
+			//else searchCategory(plist, curcount);
 		}
-		*/
+		
 	}
 		printf("\n\n종료되었습니다. 감사합니다.\n");	
 		return 0;
